dbe38ba94964fd232e76922e7ec7f87f234f533d
lbcong <lbcong15@gmail.com>
2021-05-31 19:21:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var ws = require('./ws')
 
+var port = normalizePort(process.env.PORT || 3000, () => { console.log("Express server listening on port %d", this.address().port); });
+
 var app = express()
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
